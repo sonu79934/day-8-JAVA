c7778cb8e41c1600ccafad2fc3491424e24012fe
sonu79934 <yogasritha8@gmail.com>
2024-12-09 13:51:46 +0530

    Add files via upload

---
+++ twodimentionalarray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class twodimentionalarray{
+  public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      //initialization of 2D array
+      int a[][]={{1,2},{3,4}};
+      //accepting 1D array in 2D array
+      System.out.println(Arrays.toString(a[0]));
+      System.out.println(Arrays.toString(a[1]));
+  }
+}
+
+
+
